ebdb50eaa17a191c2f3b84d3a7b71f19c1d50a18
laabk <laabkdavid@gmail.com>
2024-02-20 07:09:57 -0800

    4-number_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
-"""this process start Flask application
 """
-
+this process start Flask application
+"""
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
+def hello_hbnb():
     """
-    this content returns Hello HBNB!"""
-    return 'Hello HBNB!'
+    defines route" and returns HBNB
+    """
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
-    return 'HBNB'
+    """define hbnb route and returns HBNB
+    """
+    return "HBNB"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', po
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -35,4 +35,4 @@ def pythoniscool(text='is cool'):
     return 'Python ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', 
+    app.run(host='0.0.0.0', port=5000)
